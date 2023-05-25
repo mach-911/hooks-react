9ae514165e3f1477f4e7b6df4d1bb0fa316ea554
Marco Contreras <128122012+mach-911@users.noreply.github.com>
2023-05-25 18:38:11 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+export default function App() {
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <p> Haz clickeado {count} veces</p>
+      <button onClick={() => setCount(count + 1)}>Haz clic</button>
+    </div>
+  );
+}
